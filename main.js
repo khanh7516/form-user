a13b41de4bdf9ed6d685d738e52f2d400fb26802
KhanhNgo <62138525+khanh7516@users.noreply.github.com>
2023-10-10 19:24:38 +0700

    add gender and password rules

--- main.js
+++ main.js
@@ -1,60 +1,96 @@
 // Xử lý validate form
-$('#form').validate({
-    rules: {
-        name: {
-            required: true
-        },
-        email: {
-            required: true,
-            email: true,
-        },
-        phone: {
-            required: true,
-            pattern: /^(0\d{9}|(\+84|84)[1-9]\d{8})$/
-        },
-        description: {
-            required: true,
-            minlength: 10,
-            maxlength: 20
-        },
+$("#form").validate({
+  rules: {
+    name: {
+      required: true,
     },
-    messages: {
-        name: {
-            required: "Tên user không được để trống"
-        },
-        email: {
-            required: "Email không được để trống",
-            email: "Email không đúng định dạng"
-        },
-        phone: {
-            required: "Số điện thoại không được để trống",
-            pattern: "Số điện thoại không đúng định dạng",
-
-        },
-        description: {
-            required: "Mô tả không được để trống",
-            minlength: "Mô tả phải có ít nhất 10 ký tự",
-            maxlength: "Mô tả không được quá 20 ký tự"
-        },
+    email: {
+      required: true,
+      email: true,
     },
-    errorElement: 'span',
-    errorPlacement: function (error, element) {
-        error.addClass('invalid-feedback');
-        element.closest('.form-group').append(error);
+    phone: {
+      required: true,
+      pattern: /^(0\d{9}|(\+84|84)[1-9]\d{8})$/,
     },
-    highlight: function (element, errorClass, validClass) {
-        $(element).addClass('is-invalid');
+    description: {
+      required: true,
+      minlength: 10,
+      maxlength: 20,
     },
-    unhighlight: function (element, errorClass, validClass) {
-        $(element).removeClass('is-invalid');
-    }
+    gender: {
+      required: true,
+    },
+    password: {
+      required: true,
+      minlength: 6,
+    },
+    repassword: {
+      required: true,
+      minlength: 6,
+      passwordMatch: true, // Sử dụng quy tắc tùy chỉnh passwordMatch
+    },
+  },
+  messages: {
+    name: {
+      required: "Tên user không được để trống",
+    },
+    email: {
+      required: "Email không được để trống",
+      email: "Email không đúng định dạng",
+    },
+    phone: {
+      required: "Số điện thoại không được để trống",
+      pattern: "Số điện thoại không đúng định dạng",
+    },
+    description: {
+      required: "Mô tả không được để trống",
+      minlength: "Mô tả phải có ít nhất 10 ký tự",
+      maxlength: "Mô tả không được quá 20 ký tự",
+    },
+    gender: {
+      required: "Giới tính không được để trống",
+    },
+    password: {
+      required: "Password không được để trống",
+      minlength: "Password phải có ít nhất 6 ký tự",
+    },
+    repassword: {
+      required: "Confirm password không được để trống",
+      minlength: "Pasword phải có ít nhất 6 ký tự",
+      //   passwordMatch: "Confirm password không chính xác",
+    },
+  },
+  errorElement: "span",
+  errorPlacement: function (error, element) {
+    error.addClass("invalid-feedback");
+    element.closest(".form-group").append(error);
+  },
+  highlight: function (element, errorClass, validClass) {
+    $(element).addClass("is-invalid");
+  },
+  unhighlight: function (element, errorClass, validClass) {
+    $(element).removeClass("is-invalid");
+  },
 });
 
 // Xử lý submit form
-const formEl = document.getElementById('form');
-formEl.addEventListener('submit', (e) => {
-    e.preventDefault();
+const formEl = document.getElementById("form");
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    if (!$('#form').valid()) return;
-    alert('Submit thành công');
-})
+  if (!$("#form").valid()) return;
+  alert("Submit thành công");
+});
+
+$.validator.addMethod(
+  "passwordMatch",
+  function (value, element) {
+    // Lấy giá trị của trường "password" và "repassword"
+    var password = $("#password").val();
+    var repassword = value;
+
+    // So sánh hai giá trị
+    return password === repassword;
+  },
+  "Confirm password không chính xác"
+);
